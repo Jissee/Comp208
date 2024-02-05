ff2bbb3e023e4412ea62f9fdfa19aa58ebb558ac
jissee <2431740933@qq.com>
2024-02-05 20:53:11 +0000

    update

--- Comp208/Program.cs
+++ Comp208/Program.cs
@@ -16,7 +16,7 @@ namespace Comp208
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
