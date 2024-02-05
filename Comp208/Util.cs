cb1114e7b9b642305226f9ed933cd4743aa0bfd3
jissee <2431740933@qq.com>
2024-02-05 20:53:11 +0000

    update

---
+++ Comp208/Util.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp208
+{
+    internal static class Util
+    {
+        internal static Icon MakeIcon(Bitmap bitmap)
+        {
+            IntPtr ptr = bitmap.GetHicon();
+            return Icon.FromHandle(ptr);
+        }
+    }
+}
