5415de9641d8f9fad8fcb5635027f47e8ef07d68
jissee <2431740933@qq.com>
2024-02-22 09:51:44 +0000

    upd

---
+++ EoE/Network/PacketHandler/PacketHandler.cs
@@ -0,0 +1,58 @@
+﻿using EoE.Network.Packets;
+using EoE.Server.Network;
+using EoE.Server.Network.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EoE.Network
+{
+    public abstract class PacketHandler
+    {
+        protected static Dictionary<string, Type> packetTypes;
+        protected static Dictionary<Type, Delegate> encoders = new Dictionary<Type, Delegate>();
+        protected static Dictionary<Type, Delegate> decoders = new Dictionary<Type, Delegate>();
+
+        static PacketHandler()
+        {
+            packetTypes = new Dictionary<string, Type>();
+            Register<ClientLoginPacket>();
+            Register<NewPacket>();
+        }
+
+        public static void Register<T>() where T : IPacket<T>
+        {
+            Type type = typeof(T);
+            string packetTypeString = type.FullName;
+            if(packetTypeString == null)
+            {
+                throw new Exception("Invalid packet type");
+            }
+
+            packetTypes[packetTypeString] = type;
+
+            encoders[type] = T.Encode;
+            decoders[type] = T.Decode;
+        }
+        /// <summary>
+        /// 把从网络接收的二进制数据恢复成数据包，然后处理数据包
+        /// </summary>
+        /// <param name="data"></param>
+        public abstract void ReceivePacket(byte[] data, PacketContext context);
+
+
+        /// <summary>
+        /// 把数据包对象编码成二进制数据，然后从网络发送数据
+        /// </summary>
+        /// <param name="packet"></param>
+        public abstract void SendPacket<T>(T packet, Socket connection) where T : IPacket<T>;
+
+
+
+
+    }
+}
---
+++ EoE/Network/Packets/PacketI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EoE.Network.Packets
+{
+    internal class PacketI : IPacket<PacketI>
+    {
+        private int i;
+        public PacketI(int i)
+        {
+            this.i = i;
+        }
+
+        public void Handle(PacketContext context)
+        {
+            Console.WriteLine($"Received PacketI {i}");
+        }
+
+        public static void Encode(PacketI obj, BinaryWriter writer)
+        {
+            writer.Write(obj.i);
+        }
+
+        public static PacketI Decode(BinaryReader reader)
+        {
+            return new PacketI(reader.ReadInt32());
+        }
+    }
+}
