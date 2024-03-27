e4d1c26fa50006a5b212ddf2c07c4de3f259c09a
Lucheng Wang <sglwan19@liverpool.ac.uk>
2024-03-28 01:04:31 +0800

    a

--- EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
+++ EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9756f7c97851e9c8af015a3636ff1981658c9bec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7f75d2e25a23610f341665f7e3fd71664e7fc8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
