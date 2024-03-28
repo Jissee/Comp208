813181a58e5f0b14917183ea2f99130d8df54b43
Lucheng Wang <sglwan19@liverpool.ac.uk>
2024-03-29 00:42:32 +0800

    sss

--- EoEServer/obj/Debug/net8.0/EoE.Server.AssemblyInfo.cs
+++ EoEServer/obj/Debug/net8.0/EoE.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf1030186c68d15354b582d7b65b68776d4ae07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8fd6dcb4279e3bbae78d8caebd6acb22615066")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
