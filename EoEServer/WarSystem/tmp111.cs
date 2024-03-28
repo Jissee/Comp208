41b474650af54b5dff7e848fbc1018d90f130103
Lucheng Wang <sglwan19@liverpool.ac.uk>
2024-03-29 00:43:12 +0800

    func1

--- EoEServer/WarSystem/tmp111.cs
+++ EoEServer/WarSystem/tmp111.cs
@@ -8,7 +8,11 @@ namespace EoE.Server.WarSystem
 {
     internal class tmp111
     {
+<<<<<<< HEAD
         public void func2()
+=======
+        public void func1()
+>>>>>>> func1
         {
 
         }
