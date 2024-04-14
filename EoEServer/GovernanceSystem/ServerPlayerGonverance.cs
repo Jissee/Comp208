00e7f0c0975adf18b8b73e4c16b8fd8cf17eac4f
aye <meiqingli2023@163.com>
2024-04-14 15:21:06 +0100

    temp

--- EoEServer/GovernanceSystem/ServerPlayerGonverance.cs
+++ EoEServer/GovernanceSystem/ServerPlayerGonverance.cs
@@ -11,10 +11,7 @@ using EoE.Network;
 using EoE.WarSystem;
 using EoE.GovernanceSystem.Interface;
 using static EoE.GovernanceSystem.Interface.IGonveranceManager;
-<<<<<<< HEAD
-using EoE.GovernanceSystem.ServerInterface;
-=======
->>>>>>> 5e9381576c905c3b7aaa9a3edd817cb7749e0b97
+using EoE.GovernanceSystem.SrverInterface;
 using EoE.Network.Packets.GonverancePacket;
 using EoE.GovernanceSystem.ServerInterface;
 
