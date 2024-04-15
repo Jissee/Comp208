4159e0f996eaecbb6e8ae7a9d3d768cd732e5425
Jissee <50765103+Jissee@users.noreply.github.com>
2024-04-15 21:30:19 +0100

    nw handler

--- EoEClient/obj/Debug/net8.0-windows7.0/EoE.Client.AssemblyInfo.cs
+++ EoEClient/obj/Debug/net8.0-windows7.0/EoE.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b15f37c608464d69e68e02ba49469c435a5edcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964d9cc39fed9a958d3ce0e252dd9e8b9efa8faf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
+++ EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a75d3c2a3f939ee0eebd37c8bd662d0a00e976b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964d9cc39fed9a958d3ce0e252dd9e8b9efa8faf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
