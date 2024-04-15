bc057346f9682d761b3b2422446c97840206ea08
Jissee <50765103+Jissee@users.noreply.github.com>
2024-04-15 21:31:30 +0100

    nw fix

--- EoEServer/obj/Debug/net8.0/EoE.Server.AssemblyInfo.cs
+++ EoEServer/obj/Debug/net8.0/EoE.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964d9cc39fed9a958d3ce0e252dd9e8b9efa8faf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4159e0f996eaecbb6e8ae7a9d3d768cd732e5425")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
