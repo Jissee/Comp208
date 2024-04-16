8752b7fc940c091b80c00ea1f9512dd24049899e
Lucheng Wang <sglwan19@liverpool.ac.uk>
2024-04-17 06:08:56 +0800

    tmp

--- EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
+++ EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
@@ -14,19 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c94175f2c527b8814faacd0df679e3bbb58603e4")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186c0260f026db4324af43e4542148d1a766ab68")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f22bf46b065e571a449f04603c5ca00a943fe5")]
->>>>>>> merge
->>>>>>> merge
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0babafb91e3a09618bd30d8736c04a7c3beb307")]
->>>>>>> status
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+347eb64e3b875c7887a82ef1469b72aec4eee9fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
