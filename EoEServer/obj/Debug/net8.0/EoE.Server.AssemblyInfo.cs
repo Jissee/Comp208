0fde2be4fca099105055f47b581cc173c43c94e7
Jissee <50765103+Jissee@users.noreply.github.com>
2024-04-16 17:53:18 +0100

    merge

--- EoEServer/obj/Debug/net8.0/EoE.Server.AssemblyInfo.cs
+++ EoEServer/obj/Debug/net8.0/EoE.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef85cf893d253e165c9030ab9563be8f425e8c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba652d6a2a7b8edcf4fa8a4857439c4dc7dc95f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
