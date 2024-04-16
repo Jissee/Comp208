c4ff2692a5f375f06a00e86c27249801224bc62d
Jissee <50765103+Jissee@users.noreply.github.com>
2024-04-16 11:13:10 +0100

    merge pr50

---
+++ EoEServer/Events/EventList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EoE.Server.Events
+{
+    public class EventList : ITickable
+    {
+        private List<Event> events = new List<Event>();
+        public EventList() { }
+        public void AddEvent(Event e) 
+        {
+            events.Add(e);
+        }
+        public void Tick()
+        {
+            List<Event> removal = new List<Event>();
+            foreach (Event e in events)
+            {
+                e.Tick();
+                if (e.NeedRemove())
+                {
+                    removal.Add(e);
+                }
+            }
+            events.RemoveAll(removal.Contains);
+        }
+    }
+}
