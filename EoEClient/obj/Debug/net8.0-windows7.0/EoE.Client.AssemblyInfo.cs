dc50f14d51ee3d80a0bfb73cb1a50dffd0507aa3
Lucheng Wang <sglwan19@liverpool.ac.uk>
2024-04-17 04:51:27 +0800

    merge

--- EoEClient/obj/Debug/net8.0-windows7.0/EoE.Client.AssemblyInfo.cs
+++ EoEClient/obj/Debug/net8.0-windows7.0/EoE.Client.AssemblyInfo.cs
@@ -14,7 +14,15 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c94175f2c527b8814faacd0df679e3bbb58603e4")]
+=======
+<<<<<<< HEAD
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186c0260f026db4324af43e4542148d1a766ab68")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f22bf46b065e571a449f04603c5ca00a943fe5")]
+>>>>>>> merge
+>>>>>>> merge
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
