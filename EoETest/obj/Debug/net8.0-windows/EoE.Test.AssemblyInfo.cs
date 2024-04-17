39b599ec9e4e8bf05573e538cc7f666135cff819
Jissee <50765103+Jissee@users.noreply.github.com>
2024-04-17 22:26:57 +0100

    merge

--- EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
+++ EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d84dbea6c2033c47e6d5c2b56ce2170610a76a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f78410b7f03087cca88355abbc058741fb57194")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
