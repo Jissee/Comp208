680145b5df4b53a5cc05affc6cd4f0d059cc18b9
瑞洋 纪 <2248297990@qq.com>
2024-04-17 21:19:26 +0800

    change

--- EoE/obj/Debug/net8.0/EoE.AssemblyInfo.cs
+++ EoE/obj/Debug/net8.0/EoE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c11478fc77f4b1c74414d15153a8274cafdd7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd32ab156cd5750577fef73aee0204712b46be2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
