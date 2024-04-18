21312842b6ae1736b012f463f203de489318fc22
aye <meiqingli2023@163.com>
2024-04-18 14:27:37 +0100

    update

--- EoEClient/obj/Debug/net8.0-windows7.0/EoE.Client.AssemblyInfo.cs
+++ EoEClient/obj/Debug/net8.0-windows7.0/EoE.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65d12e76c47b5e70bbd6d526805cd53fe39146f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1a244080ac7a0bb81671b3c14ceb2f8497bc6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
