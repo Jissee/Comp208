7b071ce41c621b4c8dc0ae755de7526c6b70f1ca
Jissee <50765103+Jissee@users.noreply.github.com>
2024-05-07 16:25:33 +0100

    update test

---
+++ EoETest/TestWar.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EoE.Test
+{
+    [TestClass]
+    public class TestWar
+    {
+        [TestMethod]
+        public void TestDeclareWar()
+        {
+
+        }
+        [TestMethod]
+        public void TestSurrender()
+        {
+            Assert.AreEqual(true, false);
+        }
+    }
+}
