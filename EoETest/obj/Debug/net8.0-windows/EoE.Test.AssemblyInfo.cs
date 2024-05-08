b4a67ed4ee0fb7d8a9262fba9b5663acd6a6dc48
Jissee <50765103+Jissee@users.noreply.github.com>
2024-05-08 19:02:21 +0100

    Remove Tests

--- EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
+++ EoETest/obj/Debug/net8.0-windows/EoE.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa94f6c5771e4fe272311d5b09fa406f15c5b30f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5dc04ff166ea1d7403994f8a44109b108f0efd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EoE.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
